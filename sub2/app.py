31cef42b8a67df83def576b146ed05b8aa83bacd
jeongmyeonghyun <mh_jeong93@naver.com>
2019-08-29 15:46:53 +0900

    Edit app.py

--- sub2/app.py
+++ sub2/app.py
@@ -2,8 +2,7 @@ import pickle
 from threading import Thread
 import sqlite3
 import numpy as np
-
-from PIL import Image
+import time
 
 from konlpy.tag import Okt
 from flask import Flask
@@ -12,13 +11,12 @@ from slackeventsapi import SlackEventAdapter
 from scipy.sparse import lil_matrix
 
 # slack 연동 정보 입력 부분
-SLACK_TOKEN = "xoxb-724397827219-739240775904-5fQc8gC6RkZRx0MTFu2ol2Ia"
-SLACK_SIGNING_SECRET = "f8cdb41515e9fa9fafdcf64c57ac3850"
+SLACK_TOKEN = "xoxb-720220358483-738701955364-q3tCkTnPKzSFEQbW2a8vnrWm"
+SLACK_SIGNING_SECRET = "61d52b36a564138f59046147325dcfe4"
 
 app = Flask(__name__)
 
-slack_events_adaptor = SlackEventAdapter(
-    SLACK_SIGNING_SECRET, "/listening", app)
+slack_events_adaptor = SlackEventAdapter(SLACK_SIGNING_SECRET, "/listening", app)
 slack_web_client = WebClient(token=SLACK_TOKEN)
 
 # Req 2-2-1. pickle로 저장된 model.clf 파일 불러오기
@@ -34,9 +32,9 @@ pos_tagger = Okt()
 def preprocess(doc):
     # 토큰화
     text_docs = []
+    result = []
     result = ['/'.join(t) for t in pos_tagger.pos(doc, norm=True, stem=True)]
     text_docs += [result]
-    
     #one-hot 임베딩
     for idx in range(len(text_docs)):
         temp = [0]*len(word_indices)
@@ -45,32 +43,18 @@ def preprocess(doc):
             if word_indices.get(part)!=None:
                 temp[word_indices[part]]=1
         text_docs[idx]=temp
+
     return text_docs
-
-BADURL = Image.open('./img/BadOmpangi.gif')
-GOODURL = Image.open('./img/GoodOmpangi.gif')
-
-import json
-send_data = {
-    "attachments": [
-        {
-            
-            "imgPath": "img/prods/levisbluedemin01.jpg"
-
-
-        }
-    ]
-}
-json_data = json.dumps(send_data)
 
 # # Req 2-2-3. 긍정 혹은 부정으로 분류
 def classify(doc):
-    predict_result = clf.predict(preprocess(doc))[0]
+    text_docs = preprocess(doc)
+    predict_result = clf.predict(text_docs)[0]
+    print(clf.predict(text_docs)[0])
     if predict_result == 0.0:
-        return json_data
+        return "negative"
     else:
-        return json_data
-# print(classify("이 영화 정말 재밌다."))
+        return "positive"
     
 # # Req 2-2-4. app.db 를 연동하여 웹에서 주고받는 데이터를 DB로 저장
 
@@ -78,11 +62,14 @@ def classify(doc):
 # 챗봇이 멘션을 받았을 경우
 @slack_events_adaptor.on("app_mention")
 def app_mentioned(event_data):
-    
     channel = event_data["event"]["channel"]
     text = event_data["event"]["text"]
-    keywords = "helloooo"
+    # DB에 데이터 저장
+    save_text_to_db(text)
+    # 메세지 보내기
+    send_message(text, channel)
 
+def save_text_to_db(text):
     # db에 저장
     con = sqlite3.connect('./app.db')
     cur = con.cursor()
@@ -93,9 +80,18 @@ def app_mentioned(event_data):
     con.commit()
     cur.close()
 
+def send_message(text, ch):
+    # response = slack_web_client.files_upload(
+    #     channels=ch,
+    #     file="GoodOmpangi.gif"
+    # )
+    # assert response["ok"]
+    print(text.split("> ")[1])
+    keyword = classify(text.split("> ")[1])
+
     slack_web_client.chat_postMessage(
-        channel=channel,
-        text=keywords
+        channel=ch,
+        text=keyword
     )
 
 
